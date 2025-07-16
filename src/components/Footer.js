7ac2a5624932dadb4ff1ab2dddfc205349032649
msaleemsidd <saleemsiddiqui1996@gmail.com>
2025-07-16 07:20:35 +0500

    Update from Project to Portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,7 +48,7 @@ const Footer = () => {
                   { name: 'Home', href: '#home' },
                   { name: 'About', href: '#about' },
                   { name: 'Skills', href: '#skills' },
-                  { name: 'Projects', href: '#projects' },
+                  { name: 'Portfolio', href: '#projects' },
                   { name: 'Contact', href: '#contact' },
                 ].map((link) => (
                   <li key={link.name}>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
     { name: 'Home', href: '#home' },
     { name: 'About', href: '#about' },
     { name: 'Skills', href: '#skills' },
-    { name: 'Projects', href: '#projects' },
+    { name: 'Portfolio', href: '#projects' },
     { name: 'Contact', href: '#contact' },
   ];
 
